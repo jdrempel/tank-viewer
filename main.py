47b04a944466d1ce8a4853b3290ef9655dc9e015
Jeremy Rempel <jeremy.rempel@usask.ca>
2022-03-01 13:20:22 -0600

    move port configuration to a separate function

--- main.py
+++ main.py
@@ -22,6 +22,38 @@ BAUD = 115200
 TIMEOUT = 1
 
 
+def configure_port(arg):
+    comports = [tuple(p) for p in list(list_ports.comports())]
+    comports = [c for c in comports if regex.match(r'(.*Ard.*)|(.*Ser.*)', f'{c[1]} {c[2]}')]
+
+    if len(comports) == 0:
+        print('No available ports found, exiting...')
+        exit(1)
+
+    selection = 0
+    if arg is None:
+        print()
+        print('No port argument provided -- scanned and found the following candidate(s):')
+        for c in comports:
+            print(c)
+
+        if len(comports) > 1:
+            print('More than one Arduino device found. Please select one:')
+            for n, c in enumerate(comports):
+                print(f'{n}.', c)
+            selection = int(input('Selection:'))
+
+    else:
+        sub_comports = [c[0] for c in comports]
+        if arg not in sub_comports:
+            print(f'Port {arg} not found in list of available ports, exiting...')
+            exit(1)
+        else:
+            selection = sub_comports.index(arg)
+
+    return comports[selection][0]
+
+
 def check_port_presence(device, backoff):
     try:
         with Serial(device, BAUD, timeout=TIMEOUT) as s:
@@ -36,40 +68,14 @@ if __name__ == '__main__':
 
     parser = ArgumentParser(description='Display tank weights in real time')
     parser.add_argument('port', type=str, nargs='?')
-    parser.add_argument('max_tries', type=int, nargs='?', default=5)
+    parser.add_argument('--max-tries', dest='max_tries', type=int, nargs=1, default=5,
+                        help='number of times to attempt a serial connection (default: 5)')
+    parser.add_argument('--num-tanks', dest='num_tanks', nargs=1, default=5,
+                        help='number of presented air seeder tanks (default: 5)')
     args = parser.parse_args()
+    port = configure_port(args.port)
 
     byte_stream = BytesIO()
-
-    comports = [tuple(p) for p in list(list_ports.comports())]
-    comports = [c for c in comports if regex.match(r'(.*Ard.*)|(.*Ser.*)', f'{c[1]} {c[2]}')]
-
-    if len(comports) == 0:
-        print('No available ports found, exiting...')
-        exit(1)
-
-    selection = 0
-    if args.port is None:
-        print()
-        print('No port argument provided -- scanned and found the following candidate(s):')
-        for c in comports:
-            print(c)
-
-        if len(comports) > 1:
-            print('More than one Arduino device found. Please select one:')
-            for n, c in enumerate(comports):
-                print(f'{n}.', c)
-            selection = int(input('Selection:'))
-
-    else:
-        sub_comports = [c[0] for c in comports]
-        if args.port not in sub_comports:
-            print(f'Port {args.port} not found in list of available ports, exiting...')
-            exit(1)
-        else:
-            selection = sub_comports.index(args.port)
-
-    port = comports[selection][0]
 
     tries = 0
     max_tries = args.max_tries
@@ -81,7 +87,7 @@ if __name__ == '__main__':
             break
         print(f'Unable to communicate on port {port} ({tries}/{max_tries}')
 
-    if success is True:
+    if success:
         print(f'Established communication with device on port {port}')
     else:
         print(f'Max attempts reached, exiting...')
@@ -91,9 +97,11 @@ if __name__ == '__main__':
         while True:
             try:
                 byte_stream.write(ser.read())
-                if byte_stream.getbuffer().nbytes >= 8:
-                    pass  # But actually just read and parse it
             except serial.SerialTimeoutException:
                 print(f'Connection with device on port {port} timed out, exiting...')
+                exit(1)
+            except serial.SerialException:
+                print(f'Connection lost on port {port}, exiting...')
+                exit(1)
 
     pass
