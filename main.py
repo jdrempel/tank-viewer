e515c36bcb367a42bad3175a42b627f1d33e605a
Jeremy Rempel <jeremy.rempel@usask.ca>
2022-03-01 13:37:48 -0600

    add black formatter and run on main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from serial import Serial
 from sys import platform
 import time
 
-if 'linux' in platform:
+if "linux" in platform:
     from serial.tools import list_ports_linux as list_ports
-elif 'darwin' in platform:
+elif "darwin" in platform:
     from serial.tools import list_ports_osx as list_ports
-elif 'win32' in platform:
+elif "win32" in platform:
     from serial.tools import list_ports_windows as list_ports
 else:
     from serial.tools import list_ports_posix as list_ports
@@ -24,29 +24,33 @@ TIMEOUT = 1
 
 def configure_port(arg):
     comports = [tuple(p) for p in list(list_ports.comports())]
-    comports = [c for c in comports if regex.match(r'(.*Ard.*)|(.*Ser.*)', f'{c[1]} {c[2]}')]
+    comports = [
+        c for c in comports if regex.match(r"(.*Ard.*)|(.*Ser.*)", f"{c[1]} {c[2]}")
+    ]
 
     if len(comports) == 0:
-        print('No available ports found, exiting...')
+        print("No available ports found, exiting...")
         exit(1)
 
     selection = 0
     if arg is None:
         print()
-        print('No port argument provided -- scanned and found the following candidate(s):')
+        print(
+            "No port argument provided -- scanned and found the following candidate(s):"
+        )
         for c in comports:
             print(c)
 
         if len(comports) > 1:
-            print('More than one Arduino device found. Please select one:')
+            print("More than one Arduino device found. Please select one:")
             for n, c in enumerate(comports):
-                print(f'{n}.', c)
-            selection = int(input('Selection:'))
+                print(f"{n}.", c)
+            selection = int(input("Selection:"))
 
     else:
         sub_comports = [c[0] for c in comports]
         if arg not in sub_comports:
-            print(f'Port {arg} not found in list of available ports, exiting...')
+            print(f"Port {arg} not found in list of available ports, exiting...")
             exit(1)
         else:
             selection = sub_comports.index(arg)
@@ -54,24 +58,43 @@ def configure_port(arg):
     return comports[selection][0]
 
 
-def check_port_presence(device, backoff):
+def check_port_presence(device, backoff, timeout):
     try:
-        with Serial(device, BAUD, timeout=TIMEOUT) as s:
-            s.write(b'0xabc123')
+        with Serial(device, BAUD, timeout=timeout) as s:
+            s.write(b"0xabc123")
     except serial.PortNotOpenError:
         time.sleep(backoff)
         return False
     return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    parser = ArgumentParser(description='Display tank weights in real time')
-    parser.add_argument('port', type=str, nargs='?')
-    parser.add_argument('--max-tries', dest='max_tries', type=int, nargs=1, default=5,
-                        help='number of times to attempt a serial connection (default: 5)')
-    parser.add_argument('--num-tanks', dest='num_tanks', nargs=1, default=5,
-                        help='number of presented air seeder tanks (default: 5)')
+    parser = ArgumentParser(description="Display tank weights in real time")
+    parser.add_argument("port", type=str, nargs="?")
+    parser.add_argument(
+        "--max-tries",
+        dest="max_tries",
+        type=int,
+        nargs=1,
+        default=5,
+        help="number of times to attempt a serial connection (default: 5)",
+    )
+    parser.add_argument(
+        "--timeout",
+        dest="timeout",
+        type=float,
+        nargs=1,
+        default=1.0,
+        help="length of time in seconds to wait on serial connection before aborting (default: 1)",
+    )
+    parser.add_argument(
+        "--num-tanks",
+        dest="num_tanks",
+        nargs=1,
+        default=5,
+        help="number of presented air seeder tanks (default: 5)",
+    )
     args = parser.parse_args()
     port = configure_port(args.port)
 
@@ -82,26 +105,26 @@ if __name__ == '__main__':
     success = False
     while tries < max_tries:
         tries += 1
-        if check_port_presence(port, tries):
+        if check_port_presence(port, tries, args.timeout):
             success = True
             break
-        print(f'Unable to communicate on port {port} ({tries}/{max_tries}')
+        print(f"Unable to communicate on port {port} ({tries}/{max_tries}")
 
     if success:
-        print(f'Established communication with device on port {port}')
+        print(f"Established communication with device on port {port}")
     else:
-        print(f'Max attempts reached, exiting...')
+        print(f"Max attempts reached, exiting...")
         exit(1)
 
-    with Serial(port, BAUD, timeout=TIMEOUT) as ser:
+    with Serial(port, BAUD, timeout=args.timeout) as ser:
         while True:
             try:
                 byte_stream.write(ser.read())
             except serial.SerialTimeoutException:
-                print(f'Connection with device on port {port} timed out, exiting...')
+                print(f"Connection with device on port {port} timed out, exiting...")
                 exit(1)
             except serial.SerialException:
-                print(f'Connection lost on port {port}, exiting...')
+                print(f"Connection lost on port {port}, exiting...")
                 exit(1)
 
     pass
